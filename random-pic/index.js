bcc97fbf40518a41e6bc9d7395722165409bc08e
skaimid <zhuzhaoxin2016@gmail.com>
2023-01-07 22:01:43 +0800

    using others api

--- random-pic/index.js
+++ random-pic/index.js
@@ -1,80 +1,75 @@
 'use strict'
 const { segment } = require('icqq')
-const fs = require('fs')
 const path = require('path')
-const download = require('download')
+const Jimp = require('jimp')
 
-const keyWordsMap = [
-    {
-        keywords: ['舟', '粥', '粥粥'],
-        ch: 'ArkPics'
-    },
-    {
-        keywords: ['原', '原神', '烧鸡'],
-        ch: 'GenShinPicBed'
-    },
-    {
-        keywords: ['三次元', '3'],
-        ch: 'GFW3DS'
-    },
-    {
-        keywords: ['二次元', '2'],
-        ch: 'GfWR16'
-    },
-    {
-        keywords: ['铜'],
-        ch: 'moeisland'
-    },
-    {
-        keywords: ['猫', 'cat'],
-        ch: 'miaowu'
-    }
-]
-
-const sendRandImg = (msg, ch) => {
+async function rotate (file) {
     try {
-        download(`http://127.0.0.1:8000/tg/randomPic?name=${ch}`).then((res) => {
-            const imgpath = path.join(
-                __dirname,
-                '../../tempImg',
-                `${Date.now()}-${Math.random()}.jpg`
-            )
-            console.log(imgpath)
-            fs.writeFile(imgpath, res, (err) => {
-                if (err) {
-                    throw err
-                }
-                const responseMsg = ['吟唱开始', segment.image(imgpath)]
-                msg.reply(responseMsg)
-            })
-        })
+        const imgpath = path.join(
+            __dirname,
+            '../../tempImg',
+            `${Date.now()}-${Math.random()}.jpg`
+        )
+        // 读取图片
+        const image = await Jimp.read(file)
+        await image.rotate(1)
+        // 保存
+        await image.writeAsync(imgpath)
+        return imgpath
     } catch (e) {
         console.log(e)
-        msg.reply('出错啦')
     }
 }
 
 function listener (msg) {
+    let count = 1
+    const reply = []
     if (msg.raw_message && msg.raw_message.startsWith('#吟唱')) {
-        let sent = false
-        console.log(keyWordsMap)
-        for (const item of keyWordsMap) {
-            for (const keyword of item.keywords) {
-                if (sent) {
-                    break
-                }
-                if (msg.raw_message.indexOf(keyword) > 0) {
-                    sendRandImg(msg, item.ch)
-                    sent = true
-                    break
-                }
+        const command = msg.raw_message.split(' ')
+        // if (!command[1]) {
+        //     msg.reply('未指定类型，发送随机类型')
+        // }
+        if (command[2] && Number(command[2]) && Number.isInteger(Number(command[2]))) {
+            count = Number(command[2])
+            if (count < 1) {
+                count = 1
+                msg.reply('数量必须大于1')
             }
+            if (count > 15) {
+                count = 15
+                msg.reply('数量必须小于15')
+            }
+        } else {
+            command[2] && msg.reply(`数量 ${command[2]} 不能解析为整数，请输入正确的数字`)
         }
-        if (!sent) {
-            const index = Math.floor(Math.random() * keyWordsMap.length)
-            console.log(index)
-            sendRandImg(msg, keyWordsMap[index].ch)
-        }
+        msg.reply('吟唱准备中~')
+        fetch(`https://image.anosu.top/pixiv/json?num=${1}&keyword=${command[1]}&proxy=i.pixiv.re`)
+            .then(res => res.json())
+            .then(rs => {
+                console.log(rs)
+                if (!rs || rs.length === 0) {
+                    msg.reply(['笑死，没找到呢~你个大傻杯~xp太怪了吧~', segment.image(path.join(
+                        __dirname,
+                        '../../tempImg',
+                        'sb.jpg'
+                    ))])
+                    return
+                }
+                for (const img of rs) {
+                    console.log(img)
+                    rotate(img.url).then((path) => {
+                        msg.reply([
+                            segment.image(path),
+                            `pixiv pid ${img.pid}\n`,
+                            img.tags.reduce((pre, cur) => pre.concat(`#${cur} `), '')
+                        ])
+                    })
+                }
+                msg.reply(reply)
+            }).catch(e => {
+                console.log(e)
+                msg.reply('出错啦~')
+            })
     }
 }
 

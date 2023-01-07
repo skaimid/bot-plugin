5f543c17981d3d1176f4fc58c5d564cf57a5cf0c
skaimid <zhuzhaoxin2016@gmail.com>
2023-01-07 22:07:25 +0800

    remove voice

--- README.md
+++ README.md
@@ -14,7 +14,8 @@ https://github.com/Steve-xmh/neonbot/tree/dev
 
 问题：
 
-- [ ] 搜索图片时效性差，不准确。后续可更换api
+- [ ] 搜索图片时效性差，不准确
+- [ ] 搜索图片api以来外网，较卡顿，且不稳定
 
 ## 随机图片
 
@@ -22,5 +23,6 @@ https://github.com/Steve-xmh/neonbot/tree/dev
 
 ## 语音烂活
 
+**存在bug, 临时下线**
 - 通过关键词，发送烂梗原声
 - 定时任务发送 [神里绫华问候语音](https://wiki.biligame.com/ys/%E7%A5%9E%E9%87%8C%E7%BB%AB%E5%8D%8E%E8%AF%AD%E9%9F%B3)
--- voice/index.js
+++ voice/index.js
@@ -1,9 +1,6 @@
 'use strict'
-const { segment, record } = require('icqq')
+const { segment } = require('icqq')
 const path = require('path')
-const cron = require('node-cron')
-
-const tasks = []
 
 const anwser = {
     任何邪恶: 'xiee.mp3',
@@ -12,30 +9,32 @@ const anwser = {
     答辩: 'xiee.mp3'
 }
 
-function getRandomInt (max) {
-    return Math.floor(Math.random() * max)
-}
+// function getRandomInt (max) {
+//     return Math.floor(Math.random() * max)
+// }
 
 function listener (msg) {
     if (msg.raw_message) {
-        if (getRandomInt(5) === 1) {
-            const vpath = path.join(
-                __dirname,
-                './voice',
-                `${getRandomInt(11) + 1}.mp3`
-            )
-            msg.reply([segment.record(vpath)])
-        }
+        // if (getRandomInt(5) === 1) {
+        //     const vpath = path.join(
+        //         __dirname,
+        //         './voice',
+        //         `${getRandomInt(11) + 1}.mp3`
+        //     )
+        //     msg.reply([segment.record(vpath)])
+        // }
         for (const keyword in anwser) {
-            if (Object.hasOwnProperty.call(anwser, keyword)) {
-                const filename = anwser[keyword]
-                if (msg.raw_message.indexOf(keyword) !== -1) {
-                    const vpath = path.join(
-                        __dirname,
-                        './voice',
-                        filename
-                    )
-                    msg.reply([segment.record(vpath)])
+            const filename = anwser[keyword]
+            if (msg.raw_message.indexOf(keyword) !== -1) {
+                const vpath = path.join(
+                    __dirname,
+                    './voice',
+                    filename
+                )
+                try {
+                    msg.reply(segment.record(vpath))
+                } catch (e) {
+                    console.log(e)
                 }
             }
         }
@@ -44,40 +43,9 @@ function listener (msg) {
 
 function enable (bot) {
     bot.on('message', listener)
-    tasks.push(cron.schedule('0 8 * * *', async () => {
-        await this.sendGroupMsg('569027580', record(path.join(
-            __dirname,
-            './voice',
-            'zao.mp3'
-        )))
-    }))
-    tasks.push(cron.schedule('30 12 * * *', async () => {
-        await this.sendGroupMsg('569027580', record(path.join(
-            __dirname,
-            './voice',
-            'zhong.mp3'
-        )))
-    }))
-    tasks.push(cron.schedule('0 18 * * *', async () => {
-        await this.sendGroupMsg('569027580', record(path.join(
-            __dirname,
-            './voice',
-            'wan.mp3'
-        )))
-    }))
-    tasks.push(cron.schedule('30 22 * * *', async () => {
-        await this.sendGroupMsg('569027580', record(path.join(
-            __dirname,
-            './voice',
-            'wanan.mp3'
-        )))
-    }))
-
-    tasks.forEach(task => task.start())
 }
 function disable (bot) {
     bot.off('message', listener)
-    tasks.forEach(task => task.destroy())
 }
 
 module.exports = {
